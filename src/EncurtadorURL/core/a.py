4d5301900e43d75603a2a66d22d78a78dd33a296
Vítor Rezende <vitorez2002@gmail.com>
2025-09-09 15:20:01 -0300

    atualização da criação do primeiro usuário

--- src/EncurtadorURL/core/a.py
+++ src/EncurtadorURL/core/a.py
@@ -5,10 +5,27 @@ import psycopg2
 conn = connect_to_db()
 cur = conn.cursor()
 
-p_hash = create_hash('senha')
+username = 'usuario'
+email = 'email'
+password = 'senha'
 
-cur.execute(f"INSERT INTO USUARIO (USERNAME, EMAIL, PASSWORD_HASH) VALUES ('usuario', 'email', '{p_hash}');")
-conn.commit()
+p_hash = create_hash(password)
+
+cur.execute(
+    "SELECT COUNT(*) FROM USUARIO WHERE USERNAME = %s OR EMAIL = %s;",
+    (username, email)
+)
+count = cur.fetchone()[0]
+
+if count > 0:
+    print("Usuário já existe no banco de dados.")
+else:
+    cur.execute(
+        "INSERT INTO USUARIO (USERNAME, EMAIL, PASSWORD_HASH) VALUES (%s, %s, %s);",
+        (username, email, p_hash)
+    )
+    conn.commit()
+    print("Usuário cadastrado com sucesso!")
 
 cur.close()
 conn.close()
